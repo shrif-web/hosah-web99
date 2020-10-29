06684c0ce20f5dd8b169889f11c9f8acd4d5e03d
Ahmad Karimi <ak12hastam@gmail.com>
2020-10-29 20:56:43 +0330

    add loadtest files

---
+++ load-test/go_sha.py
@@ -0,0 +1,11 @@
+import time
+import random
+from locust import HttpUser, task
+from locust.user.wait_time import between
+
+class QuickstartUser(HttpUser):
+    wait_time = between(1, 2)
+
+    @task
+    def index_page(self):
+        self.client.post('go/sha256/', json={'num1': random.randint(0, 5), 'num2': random.randint(10, 1000)})
