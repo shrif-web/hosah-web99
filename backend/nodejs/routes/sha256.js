c987e6b8fcff84ba76689a761c7058b58750c7f8
Hossein Alimadad <aghosey@cafebazaar.ir>
2020-10-30 17:57:44 +0330

    fix(nodejs-sha256): Handle bad request

--- backend/nodejs/routes/sha256.js
+++ backend/nodejs/routes/sha256.js
@@ -5,7 +5,13 @@ const router = express.Router();
 
 router.post('/', (req, res) => {
   const { num1, num2 } = req.body;
-  const sumBitArray = sjcl.hash.sha256.hash(num1 + num2);
+
+  if (Number.isNaN(num1) || Number.isNaN(num2)) {
+    res.status(400).json({ message: 'Please enter two valid numbers' });
+    return;
+  }
+
+  const sumBitArray = sjcl.hash.sha256.hash(`${Number(num1) + Number(num2)}`);
   const sumHash = sjcl.codec.hex.fromBits(sumBitArray);
 
   res.json({ sum: sumHash });
